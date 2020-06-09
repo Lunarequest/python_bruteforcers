ffc9acd625816408e428192b1d55d3809af1614f
advaithm <advaith.madhukar@gmail.com>
2020-06-09 17:24:24 +0530

    fixed logical error

--- telnet.py
+++ telnet.py
@@ -4,13 +4,13 @@ import os
 import argparse
 
 def telconnect(host, username, password, port=23,code=0):
-    tn =  telnetlib.Telnet(host)
+    tn =  telnetlib.Telnet(host, port, timeout=5)
     tn.read_until(b"login: ")
     tn.write(username.encode("ascii")+b"\n")
     tn.read_until(b"Password: ")
     tn.write(password.encode("ascii")+b"\n")
-    out = tn.read_all(timeout=5)
+    out = tn.read_all()
     if "Connected" in out:
         code = 1
-    return code
     tn.close()
+    return code
