c27cbe8dbbc0584ba0a981522d4763b79fdb58f9
advaithm <advaith.madhukar@gmail.com>
2020-06-09 17:18:41 +0530

    created connect function

--- telnet.py
+++ telnet.py
@@ -3,8 +3,14 @@ import telnetlib
 import os 
 import argparse
 
-def telconnect(host, username, password, port=23):
+def telconnect(host, username, password, port=23,code=0):
     tn =  telnetlib.Telnet(host)
     tn.read_until(b"login: ")
     tn.write(username.encode("ascii")+b"\n")
     tn.read_until(b"Password: ")
+    tn.write(password.encode("ascii")+b"\n")
+    out = tn.read_all(timeout=5)
+    if "Connected" in out:
+        code = 1
+    return code
+    tn.close()
